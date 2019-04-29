b3d3f898d592850b2e7653a05c82f02e8579fe1b
Samuell1 <samuell.patro@gmail.com>
2019-04-29 13:42:56 +0200

    Update AssetCdn.php

--- classes/AssetCdn.php
+++ classes/AssetCdn.php
@@ -9,12 +9,14 @@ class AssetCdn
      * @param  string  $path
      * @return string
      */
-    public function assetCdn($path)
+    public function assetCdn($path): string
     {
         if (!config('cdn.use_cdn')) {
-            return asset($path);
+            $theme = themes_path();
+            return asset($theme.$path);
         }
         $cdnUrl = config('cdn.cdn_url');
+
         // Remove slashes from ending of the path
         $cdnUrl = rtrim($cdnUrl, '/');
         return $cdnUrl . '/' . trim($path, '/');
