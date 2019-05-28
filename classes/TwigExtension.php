e46eb873ac3242e97f6e56c7a6240547d23f095a
Samuell1 <samuell.patro@gmail.com>
2019-05-28 09:49:13 +0200

    Remove hotreload custom code

--- classes/TwigExtension.php
+++ classes/TwigExtension.php
@@ -57,19 +57,14 @@ class TwigExtension
         $manifest = Cache::rememberForever('cdn:manifest', function () {
             $theme = Theme::getActiveTheme();
             $manifestPath = $theme->getPath() . config('cdn.manifestPath');
-            if (config('cdn.useHotreload') && !config('cdn.active')) {
-                return $this->getHotreloadManifest($theme->getDirName());
-            } else {
-                return $this->getLocalManifest($manifestPath);
-            }
+            return $this->getLocalManifest($manifestPath);
         });
 
-        return $manifest->$path;
-    }
-
-    private function getHotreloadManifest($themeDir)
-    {
-        return json_decode($this->curl_get_contents(config('cdn.hotReloadUrl', 'http://localhost:8080').'/themes/' . $themeDir . config('cdn.manifestPath')));
+        if (isset($manifest->$path)) {
+            return $manifest->$path;
+        } else {
+            throw new SystemException('Missing '.$path.' file in manifest.json');
+        }
     }
 
     private function getLocalManifest($manifestPath)
@@ -79,19 +74,5 @@ class TwigExtension
         } else {
             throw new SystemException('Missing manifest.json file');
         }
-    }
-
-    private function curl_get_contents($url)
-    {
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_URL, $url);
-
-        $data = curl_exec($ch);
-        curl_close($ch);
-
-        return $data;
     }
 }
