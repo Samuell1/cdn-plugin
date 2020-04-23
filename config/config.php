fc0791ee1ca96566fa9458a7e71b10fd427ba197
Samuell1 <samuell.patro@gmail.com>
2020-04-23 10:11:46 +0200

    Add default config

---
+++ config/config.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+  // CDN integration
+  'active' => false,
+  'url' => 'https://cdn.mydomain.com/',
+  'assetsFolder' => '/assets/',
+
+  // Manifest integration (webpack, laravel mix)
+  'useManifest' => false,
+  'manifestPath' => '/assets/compiled/manifest.json',
+
+  // Filesystem information that will be used with sync, push, clear commands
+  'filesystem' => [
+    'disk' => 'asset-cdn',
+    'options' => []
+  ],
+
+  // Files filter
+  'files' => [
+    'ignoreDotFiles' => true,
+    'ignoreVCS' => true,
+    'include' => [
+        'paths' => [
+            //
+        ],
+        'files' => [
+            //
+        ],
+        'extensions' => [
+            //
+        ],
+        'patterns' => [
+            //
+        ],
+    ],
+    'exclude' => [
+      'paths' => [
+        //
+      ],
+      'files' => [
+          //
+      ],
+      'extensions' => [
+          //
+      ],
+      'patterns' => [
+          //
+      ],
+    ],
+  ],
+
+];
